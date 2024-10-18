12c2dd35d4c4834386eafbeae0c91a6cae38edbc
manthan99 <manthanp@leggedrobotics.com>
2024-10-18 17:19:16 +0200

    add slurm scripts

--- slurm/train_rangevit_cs.sh
+++ slurm/train_rangevit_cs.sh
@@ -16,10 +16,10 @@ echo "Preparing the dataset"
 
 module load eth_proxy
 rangevit_dir=/cluster/home/patelm/ws/rsl/rangevit
-experiment_cfg=config_nusc
+experiment_cfg=config_nusc_cs
 num_gpus=4
 output_path=/cluster/work/rsl/patelm/result/rangevit
-experiment_name=NuScenes_Dinov2_ViT-S
+experiment_name=NuScenes_CS_ViT-S
 
 cd $rangevit_dir
 
@@ -29,6 +29,6 @@ tar -xvf /cluster/scratch/patelm/Nuscenes.tar -C ${TMPDIR}
 
 echo "Cuda visible devices ${CUDA_VISIBLE_DEVICES}"
 
-python -m torch.distributed.launch --nproc_per_node=$num_gpus --master_port=63545 --use_env main.py "${experiment_cfg}.yaml" --data_root "${TMPDIR}/Nuscenes_data" --save_path "${output_path}" --pretrained_model "${rangevit_dir}/pretrained_models/dinov2_vits14_pretrain.pth" --id "${experiment_name}"
+python -m torch.distributed.launch --nproc_per_node=$num_gpus --master_port=63545 --use_env main.py "${experiment_cfg}.yaml" --data_root "${TMPDIR}/Nuscenes_data" --save_path "${output_path}" --pretrained_model "${rangevit_dir}/pretrained_models/segmenter_checkpoint.pth" --id "${experiment_name}"
 
 # python -m torch.distributed.launch --nproc_per_node=2 --master_port=63545 --use_env main.py "config_nusc.yaml" --data_root "${TMPDIR}/Nuscenes" --save_path "/cluster/work/rsl/patelm/result/rangevit" --pretrained_model "/cluster/home/patelm/ws/rsl/rangevit/pretrained_models/dinov2_vits14_pretrain.pth" --id "NuScenes_Dinov2_ViT-S"
